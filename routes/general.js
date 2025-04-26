1e477f206c357459480d3ca8e5ae04965cb00614
beniamin-hekimian <penig300404@gmail.com>
2025-04-26 18:24:36 +0300

    using async-await

--- routes/general.js
+++ routes/general.js
@@ -19,55 +19,97 @@ public_users.post('/register', (req, res) => {
   }
 });
 
-// Get the book list available in the shop
-public_users.get('/', (req, res) => {
-  return res.status(200).json(books);
+// Get the book list available in the shop (with Promise)
+public_users.get('/', async (req, res) => {
+  try {
+    const getBooks = () => {
+      return new Promise((resolve, reject) => {
+        resolve(books);
+      });
+    };
+    const bookList = await getBooks();
+    return res.status(200).json(bookList);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching books." });
+  }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', (req, res) => {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book) {
+// Get book details based on ISBN (with Promise)
+public_users.get('/isbn/:isbn', async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+
+    const getBookByISBN = (isbn) => {
+      return new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) {
+          resolve(book);
+        } else {
+          reject("Book not found");
+        }
+      });
+    };
+
+    const book = await getBookByISBN(isbn);
     return res.status(200).json(book);
-  } else {
-    return res.status(404).json({ message: "Book not found" });
+  } catch (error) {
+    return res.status(404).json({ message: error });
   }
 });
 
-// Get book details based on author
-public_users.get('/author/:author', (req, res) => {
-  const author = req.params.author.toLowerCase();
-  const matchingBooks = [];
+// Get book details based on author (with Promise)
+public_users.get('/author/:author', async (req, res) => {
+  try {
+    const author = req.params.author.toLowerCase();
 
-  for (let key in books) {
-    if (books[key].author.toLowerCase() === author) {
-      matchingBooks.push({ isbn: key, ...books[key] });
-    }
-  }
+    const getBooksByAuthor = (author) => {
+      return new Promise((resolve, reject) => {
+        const matchingBooks = [];
+        for (let key in books) {
+          if (books[key].author.toLowerCase() === author) {
+            matchingBooks.push({ isbn: key, ...books[key] });
+          }
+        }
+        if (matchingBooks.length > 0) {
+          resolve(matchingBooks);
+        } else {
+          reject("No books found by this author");
+        }
+      });
+    };
 
-  if (matchingBooks.length > 0) {
-    return res.status(200).json(matchingBooks);
-  } else {
-    return res.status(404).json({ message: "No books found by this author" });
+    const booksByAuthor = await getBooksByAuthor(author);
+    return res.status(200).json(booksByAuthor);
+  } catch (error) {
+    return res.status(404).json({ message: error });
   }
 });
 
-// Get book details based on title
-public_users.get('/title/:title', (req, res) => {
-  const title = req.params.title.toLowerCase();
-  const matchingBooks = [];
+// Get book details based on title (with Promise)
+public_users.get('/title/:title', async (req, res) => {
+  try {
+    const title = req.params.title.toLowerCase();
 
-  for (let key in books) {
-    if (books[key].title.toLowerCase() === title) {
-      matchingBooks.push({ isbn: key, ...books[key] });
-    }
-  }
+    const getBooksByTitle = (title) => {
+      return new Promise((resolve, reject) => {
+        const matchingBooks = [];
+        for (let key in books) {
+          if (books[key].title.toLowerCase() === title) {
+            matchingBooks.push({ isbn: key, ...books[key] });
+          }
+        }
+        if (matchingBooks.length > 0) {
+          resolve(matchingBooks);
+        } else {
+          reject("No books found with this title");
+        }
+      });
+    };
 
-  if (matchingBooks.length > 0) {
-    return res.status(200).json(matchingBooks);
-  } else {
-    return res.status(404).json({ message: "No books found with this title" });
+    const booksByTitle = await getBooksByTitle(title);
+    return res.status(200).json(booksByTitle);
+  } catch (error) {
+    return res.status(404).json({ message: error });
   }
 });
 
